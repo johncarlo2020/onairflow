45d1f1fc6697fe12e0422cb432ca165c1f4ef4c1
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2023-04-22 21:17:10 +0800

    fix console error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,21 +32,11 @@
                 {{ $slot }}
             </main>
         </div>
+    <livewire:scripts />
+
         <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
         <script>
 
-            // Enable pusher logging - don't include this in production
-            Pusher.logToConsole = true;
-            var pusher = new Pusher('3d6268b6d8c01cd15db7', {
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
-    </div>
-    <livewire:scripts />
-    <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
-    <script>
-        // Enable pusher logging - don't include this in production
         Pusher.logToConsole = true;
         var pusher = new Pusher('3d6268b6d8c01cd15db7', {
             cluster: 'us2'
