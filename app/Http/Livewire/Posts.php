f782d9ae81b709b438d528c95371a9cf1530c1a4
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2023-04-22 19:14:14 +0800

    fix

--- app/Http/Livewire/Posts.php
+++ app/Http/Livewire/Posts.php
@@ -7,7 +7,6 @@ use Livewire\Component;
 class Posts extends Component
 {
     public $posts;
-    public $posts2;
 
     public function render()
     {
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    {{-- <div class="py-12">
+ <div class="py-12"> 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <h1 class="text-3xl font-bold mb-6">Create Post</h1>
 
